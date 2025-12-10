f43e673a27a76383630b5860f0ddc77914b43691
devdiagon <fredericktimo@gmail.com>
2025-12-09 21:30:42 -0500

    removed unused files

--- src/main.py
+++ src/main.py
@@ -3,22 +3,16 @@ from bus.bus_factory import BusFactory
 from settings import settings
 
 def main():
-    # Rutas de los shapefiles
-    SHP_METRO = 'D:/rutas/data/temp/eje_metro_l/eje_metro_l.shp'
-    SHP_METRO_STATIONS = 'D:/rutas/data/temp/estacion_metro_a/estacion_metro_a.shp'
-    SHP_BUS_ROUTES = 'D:/rutas/data/Rutas_urbanas_convencionales/7.Rutas Urbanas Convencionales/Rutas_urbanas_convencionales.shp'
-    SHP_BUS_STOPS = 'D:/rutas/data/Paradas_DMQ/11.Paradas buses DMQ/Paradas_Buses_DMQ.shp'
-
     # ========== Crear sistema Metro ==========
     print("Creando sistema Metro...")
-    metro_factory = MetroFactory(SHP_METRO, SHP_METRO_STATIONS)
+    metro_factory = MetroFactory(settings.SHP_METRO, settings.SHP_METRO_STATIONS)
     metro_map = metro_factory.create_map()
     
     metro_map.visualize(show_names=True, title="Sistema Metro")
 
     # ========== Crear sistema Buses ==========
     print("\nCreando sistema de Buses...")
-    bus_factory = BusFactory(SHP_BUS_ROUTES, SHP_BUS_STOPS, route_code='Código_Ru')
+    bus_factory = BusFactory(settings.SHP_BUS_ROUTES, settings.SHP_BUS_STOPS, route_code='Código_Ru')
     bus_map = bus_factory.create_map()
     
     bus_map.visualize(title="Sistema de Buses Urbanos")
